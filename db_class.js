9a4d3d1695dd6782e02ff19ab9dd15969203a5f4
OCTAVIO ARFA TENENBERG <47292048@ort>
2023-05-19 10:54:29 -0300

    peliculas

--- db_class.js
+++ db_class.js
@@ -13,30 +13,67 @@ class DB {
                                  .query('SELECT * FROM Personajes WHERE Id=@pId');
         return r.recordset;
     }
-    static editar_personaje = async(per) => {
-        let r = await p.request().input('pId', per.id)
-                                 .input('pImagen', per.imagen)
-                                 .input('pNombre', per.nombre)
-                                 .input('pEdad', per.edad)
-                                 .input('pPeso', per.peso)
-                                 .input('pHistoria', per.historia)
-                                 .query('UPDATE Personajes SET Id=@pId,Imagen=@pImagen,Nombre=@pNombre,Edad=@pEdad,Peso=@pPeso,Historia=@pHistoria');
+    static get_by_nombre = async (nombre) => {
+        let r = await p.request().input('pNombre', nombre)
+                                 .query('SELECT * FROM Personajes WHERE Nombre LIKE @pNombre');
         return r.recordset;
     }
-    static agregar_personaje = async(per) => {
-        let r = await p.request().input('pImagen', per.imagen)
-                                 .input('pNombre', per.nombre)
-                                 .input('pEdad', per.edad)
-                                 .input('pPeso', per.peso)
-                                 .input('pHistoria', per.historia)
+    static get_by_edad = async (edad) => {
+        edad=parseInt(edad);
+        let r = await p.request().input('pEdad', edad)
+                                 .query('SELECT * FROM Personajes WHERE Edad=@pEdad');
+        return r.recordset;
+    }
+    static get_by_pelicula = async ({id}) => {
+        let r = await p.request().input('pId', id)
+                                 .query('SELECT Personajes.Id,Imagen,Nombre,Edad,Peso,Historia FROM Personajes INNER JOIN PersonajePorPelicula ON IdPelicula=@pId WHERE IdPersonaje=Personajes.Id');
+        return r.recordset;
+    }
+    static editar_personaje = async({id, imagen, nombre, edad, peso, historia}) => {
+        let r = await p.request().input('pId', id)
+                                 .input('pImagen', imagen)
+                                 .input('pNombre', nombre)
+                                 .input('pEdad', edad)
+                                 .input('pPeso', peso)
+                                 .input('pHistoria', historia)
+                                 .query('UPDATE Personajes SET Imagen=@pImagen,Nombre=@pNombre,Edad=@pEdad,Peso=@pPeso,Historia=@pHistoria WHERE Id=@pId');
+        return r.recordset;
+    }
+    static agregar_personaje = async({imagen, nombre, edad, peso, historia}) => {
+        let r = await p.request().input('pImagen', imagen)
+                                 .input('pNombre', nombre)
+                                 .input('pEdad', edad)
+                                 .input('pPeso', peso)
+                                 .input('pHistoria', historia)
                                  .query('INSERT INTO Personajes VALUES (@pImagen,@pNombre,@pEdad,@pPeso,@pHistoria)');
         return r.recordset;
     }
-    static delete_by_id_personajes = async(id) => {
+    static delete_by_id_personajes = async({id}) => {
         let r = await p.request().input('pId', id)
                                  .query('DELETE FROM Personajes WHERE Id=@pId');
         return r.recordset;
     }
-}
 
+    static get_all_peliculas = async () => {
+        let r = await p.query("SELECT * FROM Peliculas");
+        return r.recordset;
+    }
+    static get_by_id_peliculas = async(id) => {
+        let r = await p.request().input('pId', id).query('SELECT * FROM Peliculas WHERE Id=@pId');
+        return r.recordset;
+    }
+    static agregar_pelicula = async({imagen, titulo, fecha, calificacion}) => {
+        let r = await p.request().input('pImagen', imagen)
+                                 .input('pTitulo', titulo)
+                                 .input('pFecha', new Date(fecha))
+                                 .input('pCalificacion', calificacion)
+                                 .query('INSERT INTO Peliculas VALUES (@pImagen,@pTitulo,@pFecha,@pCalificacion)');
+        return r.recordset;
+    }
+    static delete_by_id_peliculas = async(id) => {
+        let r = await p.request().input('pId', id)
+                                 .query('DELETE FROM Peliculas WHERE Id=@pId');
+        return r.recordset;
+    }
+}
 export default DB
--- index.js
+++ index.js
@@ -7,17 +7,26 @@ const port = 3000
 app.get('/', async (req, res) => {
     res.send('TP-Personajes');
 })
-
 app.get('/personajes', async (req, res) => {
     try {
-        let r = await DB.get_all_personajes();
+        let r;
+        if(req.query.nombre != null) {
+            r = await DB.get_by_nombre(req.query.nombre);
+        }
+        else if(req.query.edad != null) {
+            r = await DB.get_by_edad(req.query.edad);
+        }
+        else if(req.query.movId != null) {
+            r = await DB.get_by_pelicula(req.query.movId);
+        } else {
+            r = await DB.get_all_personajes();
+        }
         res.json(r);
     } catch (e) {
         console.log(e);
         res.send("error al obtener personajes");
     }
 })
-
 app.get('/personajes/:id', async (req, res) => {
     try {
         let r = await DB.get_by_id_personajes(req.params.id);
@@ -27,7 +36,6 @@ app.get('/personajes/:id', async (req, res) => {
         res.send("error al obtener personaje");
     }
 })
-
 app.use(bodyParser.json());
 app.post('/personajes', async (req, res) => {
     try {
@@ -38,7 +46,6 @@ app.post('/personajes', async (req, res) => {
         res.send("error al crear personaje");
     }
 })
-
 app.put('/personajes/:id', async (req, res) => {
     try {
         let r = await DB.editar_personaje(req.body);
@@ -48,7 +55,6 @@ app.put('/personajes/:id', async (req, res) => {
         res.send("error al modificar personaje");
     }
 })
-
 app.delete('/personajes/:id', async (req, res) => {
     try {
         let r = await DB.delete_by_id_personajes(req.params.id);
@@ -59,7 +65,53 @@ app.delete('/personajes/:id', async (req, res) => {
     }
 })
 
+
+app.get('/peliculas', async (req, res) => {
+    try {
+        let r = await DB.get_all_peliculas(req.query);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al obtener peliculas");
+    }
+})
+app.get('/peliculas/:id', async (req, res) => {
+    try {
+        let r = await DB.get_by_id_peliculas(req.params.id);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al obtener pelicula");
+    }
+})
+app.post('/peliculas', async (req, res) => {
+    try {
+        let r = await DB.agregar_pelicula(req.body);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al crear pelicula");
+    }
+})
+app.put('/peliculas/:id', async (req, res) => {
+    try {
+        let r = await DB.editar_pelicula(req.body);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al modificar pelicula");
+    }
+})
+app.delete('/peliculas/:id', async (req, res) => {
+    try {
+        let r = await DB.delete_by_id_peliculas(req.params.id);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al eliminar pelicula");
+    }
+})
+
 app.listen(port, () => {
     console.log('app')
 })
-
