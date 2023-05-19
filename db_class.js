199fe3b1cc337303c00d685f3d61a036d88444a2
OCTAVIO ARFA TENENBERG <47292048@ort>
2023-05-19 08:50:31 -0300

    personajes

--- db_class.js
+++ db_class.js
@@ -5,12 +5,36 @@ let p = await sql.connect(dbconfig)
 
 class DB {
     static get_all_personajes = async () => {
-        let r = await p.query("SELECT * FROM Personajes")
+        let r = await p.query("SELECT * FROM Personajes");
         return r.recordset;
     }
     static get_by_id_personajes = async(id) => {
         let r = await p.request().input('pId', id)
-                                   .query('SELECT * FROM Personajes WHERE Id=@pId')
+                                 .query('SELECT * FROM Personajes WHERE Id=@pId');
+        return r.recordset;
+    }
+    static editar_personaje = async(per) => {
+        let r = await p.request().input('pId', per.id)
+                                 .input('pImagen', per.imagen)
+                                 .input('pNombre', per.nombre)
+                                 .input('pEdad', per.edad)
+                                 .input('pPeso', per.peso)
+                                 .input('pHistoria', per.historia)
+                                 .query('UPDATE Personajes SET Id=@pId,Imagen=@pImagen,Nombre=@pNombre,Edad=@pEdad,Peso=@pPeso,Historia=@pHistoria');
+        return r.recordset;
+    }
+    static agregar_personaje = async(per) => {
+        let r = await p.request().input('pImagen', per.imagen)
+                                 .input('pNombre', per.nombre)
+                                 .input('pEdad', per.edad)
+                                 .input('pPeso', per.peso)
+                                 .input('pHistoria', per.historia)
+                                 .query('INSERT INTO Personajes VALUES (@pImagen,@pNombre,@pEdad,@pPeso,@pHistoria)');
+        return r.recordset;
+    }
+    static delete_by_id_personajes = async(id) => {
+        let r = await p.request().input('pId', id)
+                                 .query('DELETE FROM Personajes WHERE Id=@pId');
         return r.recordset;
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 import express from "express"
 import DB from './db_class.js'
+import bodyParser from "body-parser"
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('TP-Personajes')
+app.get('/', async (req, res) => {
+    res.send('TP-Personajes');
+})
+
+app.get('/personajes', async (req, res) => {
+    try {
+        let r = await DB.get_all_personajes();
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al obtener personajes");
+    }
+})
+
+app.get('/personajes/:id', async (req, res) => {
+    try {
+        let r = await DB.get_by_id_personajes(req.params.id);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al obtener personaje");
+    }
+})
+
+app.use(bodyParser.json());
+app.post('/personajes', async (req, res) => {
+    try {
+        let r = await DB.agregar_personaje(req.body);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al crear personaje");
+    }
+})
+
+app.put('/personajes/:id', async (req, res) => {
+    try {
+        let r = await DB.editar_personaje(req.body);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al modificar personaje");
+    }
+})
+
+app.delete('/personajes/:id', async (req, res) => {
+    try {
+        let r = await DB.delete_by_id_personajes(req.params.id);
+        res.json(r);
+    } catch (e) {
+        console.log(e);
+        res.send("error al eliminar personaje");
+    }
 })
 
 app.listen(port, () => {
